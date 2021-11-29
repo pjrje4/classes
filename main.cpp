30486e86506ba4c6d2e389a25fb29766bd338f4e
pjrje4 <58542025+pjrje4@users.noreply.github.com>
2021-11-29 14:23:59 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
-e
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	while (true) { //loop
+		char input[20];
+		cout << "Enter a command (ADD, SEARCH, DELETE, QUIT): ";
+		cin >> input;
+		if (strcmp(input, "ADD") == 0) { //add students
+			addStudent(students);
+		}
+		else if (strcmp(input, "SEARCH") == 0) { //print students
+			printStudent(students);
+		}
+		else if (strcmp(input, "DELETE") == 0) { //delete students
+			deleteStudent(students);
+		}
+		else if (strcmp(input, "QUIT") == 0) { //quit program
+			return 0;
+        	}
+	}
+}
